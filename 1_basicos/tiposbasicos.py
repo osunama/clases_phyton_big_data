bbeb16bbe8a6d0aa6b1cb81dc7290c6a0d3c06ef
charioteer_5 <charioteer_5@hotmail.com>
2026-04-16 13:57:12 +0200

    Tipos

--- 1_basicos/tiposbasicos.py
+++ 1_basicos/tiposbasicos.py
@@ -23,3 +23,23 @@ nombre = "Sergio"
 apellido = 'Osuna' # también comillas simples
 mensaje = 'El niño dijo:"Qué pasa"' # uso de comillas dobles y simples
 print(mensaje)
+
+# concatenar strings Sergio Osuna 50
+nombre_completo = nombre + " " + apellido + ": " + str(edad)
+print(nombre_completo)
+
+nombre_completo_2 = f'{nombre} {apellido}: {edad}'
+print(nombre_completo_2)
+
+# Multilinea
+texto_largo = """
+Seleciona:
+[1] Sopa
+[2] Puré
+[3] Gazpacho
+
+"""
+print(texto_largo)
+
+opcion = input(texto_largo)
+print (f'La opción es {opcion}')
