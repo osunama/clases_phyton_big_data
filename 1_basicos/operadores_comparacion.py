76d2892f18f1048013b1f3ad0ce323b1489aa874
charioteer_5 <charioteer_5@hotmail.com>
2026-04-17 10:42:13 +0200

    Operadores matemáticos y de comparación

---
+++ 1_basicos/operadores_comparacion.py
@@ -0,0 +1,29 @@
+numA = 18
+numB = 5
+
+#cuando comparamos, el resultado siempre es booleano
+
+#mayor que
+resultado = numA > numB
+print(resultado)
+
+#mayor o igual que
+resultado = numA >= numB
+print(resultado)
+
+#menor que
+resultado = numA < numB
+print(resultado)
+
+#menor o igual que
+resultado = numA <= numB
+print(resultado)
+
+#Igualdad
+
+resultado = numA == numB
+print(resultado)
+
+#distinto
+resultado = numA != numB
+print(resultado)
---
+++ 1_basicos/operadores_matematicos.py
@@ -0,0 +1,37 @@
+
+numA = 10
+numB = 5
+#suma
+print(5 + 5)
+print(numA+numB)
+
+#resta
+print(6-9)
+print(numA-numB)
+
+#multiplicación
+print(3*3)
+print(numA*numA)
+
+#división
+print(54/9)
+print(numA/numB)
+
+#potencia
+print(2**3)
+print(numA**numB)
+
+#division entera (sin decimales)
+print(18//5)
+print("Resultado sin decimales:", 18//5)
+
+#operado modulo (devuelve el resto de una division)
+print(18%5)
+
+#incremento / decremento / asignación
+numero = 2
+
+numero += 2 #es lo mismo que numero= numero +2 = 4
+numero -= 1 #es lo mismo que numero= numero 1 (como ahora el valor es 4) = 3
+numero += 5 #es lo mismo que numero= numero +5 (como ahora el valor es 3) = 8
+print(numero)
