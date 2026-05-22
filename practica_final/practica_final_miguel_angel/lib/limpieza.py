1a3649d9bfb8fbcb04b6835d3010d167276d83cd
Osunama <osuna.ma.rub@gmail.com>
2026-05-22 13:38:55 +0200

    practica2205

--- practica_final/practica_final_miguel_angel/lib/limpieza.py
+++ practica_final/practica_final_miguel_angel/lib/limpieza.py
@@ -54,4 +54,148 @@ def normalizar_texto(texto):
     return (texto_corregido)
 
     
+#=============== LIMPIAR VALORES NUMÉRICOS =====================
 
+def limpiar_valor_numerico(valor):
+
+    try:    
+        valor = str(valor).strip()
+        lista = ["€", "$", "&"]
+
+        for moneda in lista:
+            valor = valor.replace(moneda, "")
+
+        valor = valor.replace(".", "")
+        valor = valor.replace(",", ".")
+
+        numero = float(valor)
+
+        # Si no tiene decimales → int
+        if numero.is_integer():
+            return int(numero)
+
+        return numero
+
+    except ValueError:
+        return None
+    
+#=============== ELIMINAR CAMPOS VACIOS =====================
+# Si uno de los campos principales está vacio, eliminarlo porque no tiene sentido mantenerlo
+def eliminar_vacios(lista, campo_critico):
+
+    for campo in lista[:]:
+        if campo[campo_critico] == "":
+            lista.remove(campo)
+
+
+#=============== NORMALIZACIÓN CATEGORÍAS =====================
+   mapeo_generos = {
+    # Rock
+    "rock":         "Rock",
+    "rokc":         "Rock",
+    "roc":          "Rock",
+    "rock":         "Rock",
+    "ROCK":         "Rock",
+
+    # Electrónica
+    "electro":      "Electrónica",
+    "electronica":  "Electrónica",
+    "Electronica":  "Electrónica",
+    "ELECTRONICA":  "Electrónica",
+    "Electrónica":  "Electrónica",
+    "Electrónic":   "Electrónica",
+
+    # Hip Hop
+    "hip hop":      "Hip Hop",
+    "hip-hop":      "Hip Hop",
+    "Hip Hop":      "Hip Hop",
+    "Hip-Hop":      "Hip Hop",
+    "HIP HOP":      "Hip Hop",
+    "HipHop":       "Hip Hop",
+
+    # Jazz
+    "jazz":         "Jazz",
+    "Jazz":         "Jazz",
+    "JAZZ":         "Jazz",
+    "Jaz":          "Jazz",
+
+    # Metal
+    "metal":        "Metal",
+    "Metal":        "Metal",
+    "METAL":        "Metal",
+    "Metall":       "Metal",
+
+    # Pop
+    "pop":          "Pop",
+    "Pop":          "Pop",
+    "POP":          "Pop",
+    "Ppo":          "Pop",
+
+    # R&B
+    "r&b":          "R&B",
+    "R&B":          "R&B",
+    "R & B":        "R&B",
+    "rnb":          "R&B",
+    "RnB":          "R&B",
+
+    # Reggaetón
+    "reggaeton":    "Reggaetón",
+    "Reggaeton":    "Reggaetón",
+    "REGGAETON":    "Reggaetón",
+    "Reggaetón":    "Reggaetón",
+    "regueton":     "Reggaetón",
+    "Reguetón":     "Reggaetón",
+
+    # Ska
+    "ska":          "Ska",
+    "Ska":          "Ska",
+    "SKA":          "Ska",
+
+    # Salsa
+    "salsa":        "Salsa",
+    "Salsa":        "Salsa",
+    "SALSA":        "Salsa",
+
+    # Techno
+    "techno":       "Techno",
+    "Techno":       "Techno",
+    "TECHNO":       "Techno",
+    "Tekno":        "Techno",
+
+    # Flamenco
+    "flamenco":     "Flamenco",
+    "Flamenco":     "Flamenco",
+    "FLAMENCO":     "Flamenco",
+    "Flamenko":     "Flamenco",
+
+    # Folk
+    "folk":         "Folk",
+    "Folk":         "Folk",
+    "FOLK":         "Folk",
+
+    # Indie
+    "indie":        "Indie",
+    "Indie":        "Indie",
+    "INDIE":        "Indie",
+    "Indy":         "Indie",
+
+    # Cumbia
+    "cumbia":       "Cumbia",
+    "Cumbia":       "Cumbia",
+    "CUMBIA":       "Cumbia",
+    "Kunbia":       "Cumbia",
+}
+
+def normalizar_categoria(valor, diccionario_mapeo):
+    valor_limpio = valor.lower().strip()
+    if valor_limpio in diccionario_mapeo:
+
+        print (diccionario_mapeo[valor_limpio])
+
+    else:
+
+        print(f"AVISO: valor no reconocido → '{valor}'")
+
+        print (valor)
+
+normalizar_categoria("e", mapeo_generos)
--- practica_final/practica_final_miguel_angel/main.py
+++ practica_final/practica_final_miguel_angel/main.py
@@ -1,27 +1,96 @@
 from lib.carga import cargar_csv, cargar_excel, cargar_json, cargar_xml
 
-from lib.limpieza import limpiar_texto, normalizar_texto
+from lib.limpieza import limpiar_texto, normalizar_texto, limpiar_valor_numerico, eliminar_vacios, normalizar_categoria
 
 artistas = cargar_csv('datos', 'artistas.csv') 
 
-cargar_excel('datos', 'escenarios_horarios.xlsx')
+escenarios_horarios = cargar_excel('datos', 'escenarios_horarios.xlsx')
 
-cargar_json('datos', 'ventas_entradas.json')
+ventas_entradas = cargar_json('datos', 'ventas_entradas.json')
 
-cargar_xml('datos', 'patrocinadores.xml')
-
-
-print(limpiar_texto("     jkh     jhnng    "))
-
-print(normalizar_texto("    jose jose jóse"))
+patrocinadores = cargar_xml('datos', 'patrocinadores.xml')
 
 
+# print(limpiar_texto("     jkh     jhnng    "))
 
-# def procesar(lista):
+# print(normalizar_texto("    jose jose jóse"))
+
+# print(limpiar_valor_numerico("150.000$"))
+
+# eliminar_vacios()
+
+
+
+
+# id_artista,nombre,genero_musical,pais,cache_eur,email_manager,telefono
+def procesar_artistas(lista):
 #     ## recorrer la lista y a cada item
-#          # 
-#          # limpiezaitem['nombre]
-#     for item in lista:
-#         item
+#      # limpiezaitem['nombre]
+    lista_limpia=[]
+    for item in lista:
+        item_limpio= {
+            "id_artista": item['id_artista'],
+            "nombre": normalizar_texto(item['nombre']),
+            "genero_musical": normalizar_texto(item['genero_musical']),
+            "pais": normalizar_texto(item['pais']),
+            "cache_eur": normalizar_texto(item['cache_eur']),
+            "email_manager": normalizar_texto(item['email_manager']),
+            "telefono": normalizar_texto(item['telefono'])
+        }
+        lista_limpia.append(item_limpio)
+    return lista_limpia
 
+artistas_limpio = procesar_artistas(artistas)
 
+def procesar_escenarios(lista):
+    lista_limpia=[]
+    for item in lista:
+        item_limpio= {
+            "fecha": item['fecha'],
+            "escenario": normalizar_texto(item['escenario']),
+            "artista": normalizar_texto(item['artista']),
+            "hora_inicio": normalizar_texto(item['hora_inicio']),
+            "hora_fin": normalizar_texto(item['hora_fin']),
+            "soundcheck": normalizar_texto(item['soundcheck']),
+            "notas": normalizar_texto(item['notas'])
+        }
+        lista_limpia.append(item_limpio)
+    return lista_limpia
+
+escenarios_horarios_limpio = procesar_escenarios(escenarios_horarios)
+
+def procesar_patrocinadores(lista):
+    lista_limpia=[]
+    for item in lista:
+        item_limpio= {
+            "nombre_empresa": item['nombre_empresa'],
+            "contacto": normalizar_texto(item['contacto']),
+            "email": normalizar_texto(item['email']),
+            "importe_patrocinio": normalizar_texto(item['importe_patrocinio']),
+            "categoria": normalizar_texto(item['categoria']),
+            "fecha_inicio": normalizar_texto(item['fecha_inicio']),
+            "fecha_fin": normalizar_texto(item['fecha_fin'])
+        }
+        lista_limpia.append(item_limpio)
+    return lista_limpia
+
+patrocinadores_limpio = procesar_patrocinadores(patrocinadores)
+
+def procesar_entradas(lista):
+    lista_limpia=[]
+    for item in lista:
+        item_limpio= {
+            "id_venta": item['id_venta'],
+            "nombre_comprador": normalizar_texto(item['nombre_comprador']),
+            "email": normalizar_texto(item['email']),
+            "dni": normalizar_texto(item['dni']),
+            "tipo_entrada": normalizar_texto(item['tipo_entrada']),
+            "precio": normalizar_texto(item['precio']),
+            "fecha_compra": normalizar_texto(item['fecha_compra']),
+            "metodo_pago": normalizar_texto(item['metodo_pago'])
+
+        }
+        lista_limpia.append(item_limpio)
+    return (lista_limpia)
+
+venta_entradas_limpio = procesar_entradas(ventas_entradas)
